69f9048eb1336e5ab8fdf0561fce8da304a47a12
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-13 23:44:22 +0700

    add halaman Home, About, dan Posts
    - create tiga file view baru: home.blade.php, about.blade.php, dan posts.blade.php
    - Konfigurasi route agar dapat mengakses halaman-halaman tersebut
    - create berkas CSS, JS, dan gambar di direktori public
    - create array dengan data nama, email, dan gambar pada routes about

---
+++ belajar-laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Belajar Laravel | ABOUT</title>
+</head>
+<body>
+    <h1>HALAMAN ABOUT</h1>
+    <h3><?= $name; ?></h3>
+    <p><?= $email; ?></p>
+    <img src="img/profile.jpg" alt="Fery-Eka-Mahendra">
+</body>
+</html>
--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Fery Eka Mahendra",
+        "email" => "ferrh.mahendra@gmail.com",
+        "image" => "profile.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
