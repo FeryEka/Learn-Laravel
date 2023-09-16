5665860857ea8428216c3177e41514b520b591bc
FeryEka <Ferrh.mahendra@gmail.com>
2023-09-16 16:29:52 +0700

    Pembuatan Model, Migration, Pengisian Data, dan Route Model Binding
    - Membuat model Post.php untuk mengelola data postingan.
    - Membuat migrasi untuk tabel post guna menentukan struktur basis data.
    - Menggunakan mass assignment untuk mengisi data ke dalam tabel post secara efisien.
    - Menerapkan route model binding untuk mengaitkan model Post dengan rute aplikasi.

--- belajar-laravel/app/Models/Post.php
+++ belajar-laravel/app/Models/Post.php
@@ -2,29 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "Fery Eka Mahendra",
-        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis sunt, debitis animi reprehenderit nam sed vel quisquam eveniet vitae sequi molestias quam mollitia iusto nobis amet dicta voluptas asperiores! Eius, placeat ipsam aliquam et dolorem minima maxime veritatis commodi consequatur ut iure cum amet numquam deserunt esse non? Nisi, suscipit tempore! Ea quos nam est ut voluptatibus, voluptas, facilis impedit eius quia, dignissimos officiis dolorum tenetur consequatur! Consequuntur porro ipsa, amet quaerat minima corrupti unde aliquam eligendi, quas, reiciendis quae!"
-        ],
-        [
-        "title" => "Judul Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "Fery Eka Mahendra",
-        "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat vitae distinctio inventore minus odio provident atque ex est ducimus consectetur accusamus unde alias, sapiente exercitationem aperiam in, magni sit voluptatum facilis, veniam consequatur reiciendis! Tempore repellat possimus explicabo consequatur perferendis cumque similique eveniet mollitia, modi perspiciatis dolores iure officia vero dicta aut facilis distinctio dolorum ullam ipsa accusamus consequuntur deserunt nobis assumenda in. Consequuntur nobis aspernatur beatae nemo. Aliquam praesentium porro vel omnis? Voluptatibus, totam. Aperiam nostrum tenetur, sint voluptate inventore pariatur perspiciatis animi minus ducimus. Nihil eius deleniti quisquam aliquid labore impedit asperiores, aliquam veniam quo nulla, inventore deserunt!"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all(){
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug){
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'slug', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- belajar-laravel/resources/views/posts.blade.php
+++ belajar-laravel/resources/views/posts.blade.php
@@ -3,11 +3,8 @@
     @section('container')
         @foreach ($posts as $post)
         <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
-            </h2>
-            <h5>By: {{ $post["author"] }}</h5>
-            <p>{{ $post["body"] }}</p>
+            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->excerpt }}</p>
         </article>
         @endforeach
     @endsection
